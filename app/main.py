7725d2e4f119a0f803b02948e3b014d401a5b8d6
Nunnito Nevermind <nunnito@pm.me>
2022-05-12 10:05:28 -0500

    Add await to answer()

--- app/main.py
+++ app/main.py
@@ -146,7 +146,7 @@ async def on_callback_query(client: Client, callback_query: CallbackQuery):
         id_hash = data.split("_")[-1]
         await delete_forwarder(message, id_hash, 2)
 
-    callback_query.answer()
+    await callback_query.answer()
 
 
 async def menu(message: Message, edit: bool = True) -> None:
